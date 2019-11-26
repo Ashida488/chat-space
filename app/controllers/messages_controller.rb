55a2ef5fb9200ec655a6ae1527c151d1227e822c
Asihda488 <akgno.12612@gmail.com>
2019-11-26 22:00:24 +0900

    add messages_controller.rb

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,16 @@
 class MessagesController < ApplicationController
+  before_action :set_group
   def index
   end
+  def create
+  end
+  
+  private
+
+  def message_params
+  end
+
+  def set_group
+  end
+
 end
